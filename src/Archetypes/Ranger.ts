b43a794bba9d2cf51ce1d079801d1f743a1ae323
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2022-04-11 16:31:24 -0300

    :card_file_box: add new Archetypes

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Ranger extends Archetype {
+  private _energyType: EnergyType; 
+  private static _createdInstances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    this._createdInstances += 1;
+    return this._createdInstances;
+  }
+}
+
+export default Ranger;
